fcc74bd31f3845c4466b29600e84f7ec6baf229f
igorpompeo <pompbass@gmail.com>
2019-05-07 11:18:52 -0300

    Update user.service.ts

--- pag-teste-progresso/src/app/login/user.service.ts
+++ pag-teste-progresso/src/app/login/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  
+  login(login, senha){
+    return this.http.post('/login', {
+      login, senha
+    })
+    
+  }
 }
